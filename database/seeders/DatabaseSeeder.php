d74f97d6933bf1e33e4e8901b71a73875e6b079f
R. Kukuh <rkukuh@gmail.com>
2025-06-16 16:06:53 +0700

    Define fake data where each user has posts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Post;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users = User::factory(10)
+            ->has(Post::factory(rand(2, 10)))
+            ->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $admin = User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'admin@laracast-forum.com',
         ]);
     }
 }
