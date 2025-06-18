75ee41e086ae17046ea3eb44a90add6d805eaeda
R. Kukuh <rkukuh@gmail.com>
2025-06-18 08:13:08 +0700

    Pull post data in simplest way possible

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
     public function index()
     {
         return Inertia::render('Posts/Index', [
-            'posts' => null,
+            'posts' => Post::all(),
         ]);
     }
 
--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -1,9 +1,15 @@
 <template>
     <AppLayout>
-        This is the Posts Index Page
+        <ul>
+            <li v-for="post in posts" :key="post.id">
+                {{ post.title }}
+            </li>
+        </ul>
     </AppLayout>
 </template>
 
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
+
+defineProps(['posts']);
 </script>
