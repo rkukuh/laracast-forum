8e034d3520a43cf95f09d582050ce4345713c211
R. Kukuh <rkukuh@gmail.com>
2025-06-23 21:10:50 +0700

    Expand to show the remaining necessary model's attributes

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -18,6 +18,9 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->when($this->id === $request->user()?->id, $this->email),
+            'profile_photo_url' => $this->profile_photo_url,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
         ];
     }
 }
