0e65ddab9a3ec43effbdd07494f1f7f72504a6b3
R. Kukuh <rkukuh@gmail.com>
2025-06-25 16:21:36 +0700

    Revert back to before demonstration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +21,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
-        Model::preventLazyLoading();
     }
 }
