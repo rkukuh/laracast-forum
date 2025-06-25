1a3be2a051ca6fef9e099d5c8dad769a22ee26c8
R. Kukuh <rkukuh@gmail.com>
2025-06-25 11:50:14 +0700

    Use paginate() instead to get pagination data structure

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
     public function index()
     {
         return Inertia::render('Posts/Index', [
-            'posts' => PostResource::collection(Post::all()),
+            'posts' => PostResource::collection(Post::paginate()),
         ]);
     }
 
