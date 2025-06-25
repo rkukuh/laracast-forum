10e076c81654ce1827771bd4914b17133524d2bf
R. Kukuh <rkukuh@gmail.com>
2025-06-25 15:28:01 +0700

    Make the pagination works

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -1,60 +1,55 @@
 <!-- https://tailwindcss.com/plus/ui-blocks/application-ui/navigation/pagination -->
 <template>
-    <div class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+  <div class="flex items-center justify-between border-t border-gray-200 px-4 py-3 sm:px-6">
       <div class="flex flex-1 justify-between sm:hidden">
-        <a href="#" class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</a>
-        <a href="#" class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</a>
+          <a :href="previousUrl"
+             class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</a>
+          <a :href="nextUrl"
+             class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</a>
       </div>
       <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-        <div>
-          <p class="text-sm text-gray-700">
-            Showing
-            {{ ' ' }}
-            <span class="font-medium">{{ meta.from }}</span>
-            {{ ' ' }}
-            to
-            {{ ' ' }}
-            <span class="font-medium">{{ meta.to }}</span>
-            {{ ' ' }}
-            of
-            {{ ' ' }}
-            <span class="font-medium">{{ meta.total }}</span>
-            {{ ' ' }}
-            results
-          </p>
-        </div>
-        <div>
-          <nav class="isolate inline-flex -space-x-px rounded-md shadow-xs" aria-label="Pagination">
-            <a href="#" class="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-              <span class="sr-only">Previous</span>
-              <ChevronLeftIcon class="size-5" aria-hidden="true" />
-            </a>
-            <!-- Current: "z-10 bg-indigo-600 text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" -->
-            <a href="#" aria-current="page" class="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">1</a>
-            <a href="#" class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0">2</a>
-            <a href="#" class="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex">3</a>
-            <span class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-gray-300 ring-inset focus:outline-offset-0">...</span>
-            <a href="#" class="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex">8</a>
-            <a href="#" class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0">9</a>
-            <a href="#" class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0">10</a>
-            <a href="#" class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-              <span class="sr-only">Next</span>
-              <ChevronRightIcon class="size-5" aria-hidden="true" />
-            </a>
-          </nav>
-        </div>
+          <div>
+              <p class="text-sm text-gray-700">
+                  Showing
+                  {{ ' ' }}
+                  <span class="font-medium">{{ meta.from }}</span>
+                  {{ ' ' }}
+                  to
+                  {{ ' ' }}
+                  <span class="font-medium">{{ meta.to }}</span>
+                  {{ ' ' }}
+                  of
+                  {{ ' ' }}
+                  <span class="font-medium">{{ meta.total }}</span>
+                  {{ ' ' }}
+                  results
+              </p>
+          </div>
+          <div>
+              <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm bg-white" aria-label="Pagination">
+                  <Link 
+                        :href="link.url ?? ''" as="button" :disabled="!link.url"
+                        v-for="link in meta.links"
+                        v-html="link.label"
+                        class="relative inline-flex items-center first-of-type:rounded-l-md last-of-type:rounded-r-md px-3 py-2"
+                        :class="{
+                          'z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600': link.active,
+                         'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0': !link.active
+                        }"
+                        preserve-scroll
+                  ></Link>
+              </nav>
+          </div>
       </div>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/20/solid'
+  </div>
+</template>
 
-  defineProps(['meta']);
-  
-  const items = [
-    { id: 1, title: 'Back End Developer', department: 'Engineering', type: 'Full-time', location: 'Remote' },
-    { id: 2, title: 'Front End Developer', department: 'Engineering', type: 'Full-time', location: 'Remote' },
-    { id: 3, title: 'User Interface Designer', department: 'Design', type: 'Full-time', location: 'Remote' },
-  ]
-  </script>
+<script setup>
+import {Link} from "@inertiajs/vue3";
+import {computed} from "vue";
+
+const props = defineProps(['meta']);
+
+const previousUrl = computed(() => props.meta.links[0].url);
+const nextUrl = computed(() => [...props.meta.links].reverse()[0].url);
+</script>
