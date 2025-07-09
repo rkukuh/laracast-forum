c705cd36c7a039168d598b383e35f360665ec7a6
R. Kukuh <rkukuh@gmail.com>
2025-07-09 10:16:47 +0700

    Display a single post using new Inertia component

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -40,7 +40,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        return inertia('Posts/Show', []);
     }
 
     /**
---
+++ resources/js/Pages/Posts/Show.vue
@@ -0,0 +1,10 @@
+<template>
+    <AppLayout>
+        Hello Post Show
+    </AppLayout>
+</template>
+
+<script setup lang="ts">
+import AppLayout from "@/Layouts/AppLayout.vue";
+
+</script>
