afcc0e613cdcab02f155121a6502b2b7d919b711
R. Kukuh <rkukuh@gmail.com>
2025-07-09 09:57:42 +0700

    Add test for displaying a single post with Inertia component assertion

---
+++ tests/Feature/Controllers/PostController/ShowTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Models\Post;
+use function Pest\Laravel\get;
+
+it('can show a post', function () {
+    $post = Post::factory()->create();
+
+    get(route('posts.show', $post))
+        ->assertComponent('Posts/Show');
+});
