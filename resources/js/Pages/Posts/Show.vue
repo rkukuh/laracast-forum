55da4e16a6843830aa6fd8ed4847dd0c0f8369bc
R. Kukuh <rkukuh@gmail.com>
2025-07-09 10:43:42 +0700

    Enhance `Show.vue` to display post details and accept `post` as prop

--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -1,10 +1,14 @@
 <template>
-    <AppLayout>
-        Hello Post Show
+    <AppLayout :title="post.title">
+        <h1>{{ post.title }}</h1>
+        <span>{{ post.created_at }}</span>
+
+        <article>{{ post.body }}</article>
     </AppLayout>
 </template>
 
 <script setup lang="ts">
 import AppLayout from "@/Layouts/AppLayout.vue";
 
+const props = defineProps(['post']);
 </script>
