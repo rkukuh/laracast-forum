87f55d016910bc023c4bae394faef3b9c4bee52f
R. Kukuh <rkukuh@gmail.com>
2025-07-10 22:52:22 +0700

    Create hover effect on the whole individual post rows

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -2,19 +2,22 @@
     <AppLayout>
         <Container>
             <ul class="divide-y">
-                <li class="px-2 py-4"
-                    v-for="post in posts.data"
+                <li v-for="post in posts.data"
                     :key="post.id">
 
-                    <Link :href="route('posts.show', post.id)">
-                        <span class="font-bold text-lg">
+                    <Link
+                        class="block group px-2 py-4"
+                        :href="route('posts.show', post.id)">
+
+                        <span class="font-bold text-lg group-hover:text-indigo-500">
                             {{ post.title }}
                         </span>
 
-                        <span class="block mt-1 text-sm text-gray-500">
+                        <span class="block pt-1 text-sm text-gray-500">
                             {{ formattedDate(post) }}
                             by {{ post.user.name }}
                         </span>
+
                     </Link>
                 </li>
             </ul>
