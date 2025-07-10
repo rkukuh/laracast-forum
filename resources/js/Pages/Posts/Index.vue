3288924c2c2926046b559f1a54644328b3d5b282
R. Kukuh <rkukuh@gmail.com>
2025-07-10 22:41:24 +0700

    Show posts’ created date and its author

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -10,6 +10,11 @@
                         <span class="font-bold text-lg">
                             {{ post.title }}
                         </span>
+
+                        <span class="block mt-1 text-sm text-gray-500">
+                            {{ formattedDate(post) }}
+                            by {{ post.user.name }}
+                        </span>
                     </Link>
                 </li>
             </ul>
@@ -24,6 +29,13 @@ import AppLayout from '@/Layouts/AppLayout.vue';
 import Container from '@/Components/Container.vue';
 import Pagination from '@/Components/Pagination.vue';
 import {Link} from "@inertiajs/vue3";
+import {formatDistance, parseISO} from "date-fns";
 
 defineProps(['posts']);
+
+const formattedDate = (post) => {
+    return formatDistance(parseISO(post.created_at), new Date(), {
+        addSuffix: true,
+    })
+}
 </script>
