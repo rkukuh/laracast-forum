9851aa3c84459e9e5b858f01f2d678f349aff308
R. Kukuh <rkukuh@gmail.com>
2025-07-15 09:37:16 +0700

    Create a test case

--- tests/Feature/Controllers/PostController/ShowTest.php
+++ tests/Feature/Controllers/PostController/ShowTest.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Resources\CommentResource;
 use App\Http\Resources\PostResource;
+use App\Models\Comment;
 use App\Models\Post;
 use function Pest\Laravel\get;
 
@@ -18,4 +20,14 @@ it('passes the post to the view', function () {
 
     get(route('posts.show', $post))
         ->assertHasResource('post', PostResource::make($post));
+});
+
+it('passes comments to the view', function () {
+    $this->withoutExceptionHandling();
+
+    $post = Post::factory()->create();
+    $comments = Comment::factory(5)->for($post)->create();
+
+    get(route('posts.show', $post))
+        ->assertHasPaginatedResource('comments', CommentResource::collection($comments));
 });
