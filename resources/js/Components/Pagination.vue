7b61a100fab0048c2f1626e6c1eab07b92ac4f3c
R. Kukuh <rkukuh@gmail.com>
2025-07-16 08:04:26 +0700

    Make the `:only` prop reusable across the project

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -36,7 +36,7 @@
                           'z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600': link.active,
                          'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0': !link.active
                         }"
-                        :only="['comments']"
+                        :only="only"
                         preserve-scroll
                   ></Link>
               </nav>
@@ -49,7 +49,16 @@
 import {Link} from "@inertiajs/vue3";
 import {computed} from "vue";
 
-const props = defineProps(['meta']);
+const props = defineProps({
+    meta: {
+        type: Object,
+        required: true
+    },
+    only: {
+        type: Array,
+        default: () => []
+    }
+});
 
 const previousUrl = computed(() => props.meta.links[0].url);
 const nextUrl = computed(() => [...props.meta.links].reverse()[0].url);
