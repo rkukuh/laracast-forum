d31273f01b529ef87dfab18d11c8bcba8e1a636e
R. Kukuh <rkukuh@gmail.com>
2025-07-17 10:40:57 +0700

    Make the test case pass

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
@@ -26,9 +27,13 @@ class CommentController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Post $post)
     {
-        //
+        $comment = Comment::make($request->all());
+
+        $comment->user()->associate($request->user());
+        $comment->post()->associate($post);
+        $comment->save();
     }
 
     /**
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -11,6 +11,10 @@ class Comment extends Model
     /** @use HasFactory<\Database\Factories\CommentFactory> */
     use HasFactory;
 
+    protected $fillable = [
+        'body',
+    ];
+
     public function post(): BelongsTo
     {
         return $this->belongsTo(Post::class);
