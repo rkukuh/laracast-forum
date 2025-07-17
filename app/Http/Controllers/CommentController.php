775c371e6eb788c528afc84cf7ffbca8233d38be
R. Kukuh <rkukuh@gmail.com>
2025-07-17 16:46:03 +0700

    Clean up the controller

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -29,15 +29,14 @@ class CommentController extends Controller
      */
     public function store(Request $request, Post $post)
     {
-        $request->validate([
+        $data = $request->validate([
             'body' => ['required', 'string', 'max:255'],
         ]);
 
-        $comment = Comment::make($request->all());
-
-        $comment->user()->associate($request->user());
-        $comment->post()->associate($post);
-        $comment->save();
+        Comment::make($data)
+            ->user()->associate($request->user())
+            ->post()->associate($post)
+            ->save();
 
         return to_route('posts.show', $post);
     }
